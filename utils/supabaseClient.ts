c3f9e253df39662a89ca9c7056d4687ba0c9663c
J.AADOR <j.aador@hotmail.com>
2025-08-06 17:20:15 +0400

    Updated feature X

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -3,9 +3,9 @@ import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 import { fetch } from 'cross-fetch';
 
-const SUPABASE_URL = 'https://wplzxpodnfsxtdguwpsl.supabase.co';
+const SUPABASE_URL = 'https://wrmkuzoxnjdvlhjmtwet.supabase.co';
 const SUPABASE_ANON_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwbHp4cG9kbmZzeHRkZ3V3cHNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyODE4MDMsImV4cCI6MjA2ODg1NzgwM30.IIQw5dTdxBwzXNVoRhpeA0uDU6qyIQYsrKiOjDDLLBU';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndybWt1em94bmpkdmxoam10d2V0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ0MjQ0OTcsImV4cCI6MjA3MDAwMDQ5N30.N69ve-MhbWjGa4pPNgEdP2Ixn5LjhqCjBt8fAV2kXKI';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   global: { fetch },
